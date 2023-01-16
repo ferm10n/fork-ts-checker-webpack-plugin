8be4104822c11f77aa4dff383e19bc0d7c41a6c6
John Sanders <jws121295@gmail.com>
2023-01-16 13:36:55 -0500

    test: create repro for https://github.com/TypeStrong/fork-ts-checker-webpack-plugin/issues/557

--- examples/ts-loader-monorepo/packages/main/src/index.ts
+++ examples/ts-loader-monorepo/packages/main/src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world');
+import { ProbablyANumber } from 'fork-ts-checker-webpack-plugin-ts-loader-monorepo-example-util/src';
+
+// try changing the ProbablyANumber type in the util package
+const num: ProbablyANumber = 'five';
+console.log('Hello world', num);
